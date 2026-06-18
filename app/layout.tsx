7540fe65c05f68532e72980c13a15ba6eb546dce
Pratik Maru <pratikmaru012210@gmail.com>
2026-06-18 10:25:05 +0530

    add speed metric

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           <main className="flex-grow">
             {children}
           </main>
+          <SpeedInsights />
         </ClerkProvider>
       </body>
     </html>
